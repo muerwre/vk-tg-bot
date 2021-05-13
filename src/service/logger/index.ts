f9fbf56415c95e649cb92ed8843435d93c7f3961
Fedor Katurov <gotham48@gmail.com>
2021-05-13 15:51:20 +0700

    #5 added logger timestamps

--- src/service/logger/index.ts
+++ src/service/logger/index.ts
@@ -1,13 +1,20 @@
 import { createLogger, format, transports } from "winston";
 import prepareConfig from "../../config";
+import { keys } from "ramda";
 
 const config = prepareConfig();
 
 const logger = createLogger({
-  transports: new transports.Console({
-    format: format.simple(),
-    level: config.logger?.level || "info",
-  }),
+  transports: new transports.Console(),
+  format: format.combine(
+    format.timestamp(),
+    format.printf(
+      ({ timestamp, level, message, ...rest }) =>
+        `${timestamp} ${level}: ${message} ` +
+        (rest !== undefined && keys(rest).length ? JSON.stringify(rest) : "")
+    )
+  ),
+  level: config.logger?.level || "info",
 });
 
 export default logger;
